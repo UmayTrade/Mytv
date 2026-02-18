481ab0694e66cb5a2f32bb0c175c44214ebe5a3e
UmayTrade <kocatepe828@gmail.com>
2026-02-18 03:28:50 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalPlugin.kt
@@ -1,42 +1,35 @@
 package com.keyiflerolsun
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.webkit.CookieManager
+import android.webkit.WebSettings
+import android.webkit.WebView
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Response
+import okhttp3.*
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 @CloudstreamPlugin
 class DiziPalPlugin : Plugin() {
 
     override fun load(context: Context) {
 
-        // 🌍 GLOBAL HEADER INJECTOR
-        app.baseClient = app.baseClient.newBuilder()
-            .addInterceptor(GlobalHeaderInterceptor())
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
+        cookieManager.setAcceptThirdPartyCookies(WebView(context), true)
+
+        val client = app.baseClient.newBuilder()
+            .cookieJar(JavaNetCookieJar(java.net.CookieManager()))
+            .addInterceptor(GlobalInterceptor(context))
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
             .build()
+
+        app.baseClient = client
 
         registerMainAPI(DiziPal())
-    }
-}
-
-class GlobalHeaderInterceptor : Interceptor {
-
-    private val mainUrl = "https://dizipal1539.com"
-
-    override fun intercept(chain: Interceptor.Chain): Response {
-
-        val original = chain.request()
-
-        val request = original.newBuilder()
-            .header("User-Agent", USER_AGENT)
-            .header("Referer", mainUrl)
-            .header("Origin", mainUrl)
-            .header("Accept", "*/*")
-            .header("Connection", "keep-alive")
-            .build()
-
-        return chain.proceed(request)
     }
 }
