f9dbe5dfbf8fd6e22bf214cad8e337343378cf92
UmayTrade <kocatepe828@gmail.com>
2026-02-24 19:05:08 +0300

    Update BelgeselX.kt

--- BelgeselIzlesene/src/main/kotlin/com/umaytrade/BelgeselX.kt
+++ BelgeselIzlesene/src/main/kotlin/com/umaytrade/BelgeselX.kt
@@ -1,14 +1,12 @@
-package com.cloudstream3
+package com.umaytrade
 
-import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import org.jsoup.nodes.Element
 
-class BelgeselIzlesene : MainAPI() {
+class BelgeselX : MainAPI() {
     override var mainUrl = "https://www.belgeselizlesene.com"
     override var name = "Belgesel İzlesene"
     override val hasMainPage = true
@@ -55,31 +53,24 @@ class BelgeselIzlesene : MainAPI() {
         val description = document.selectFirst(".entry-content p")?.text()?.trim()
         val year = document.selectFirst(".published")?.text()?.split(" ")?.last()?.toIntOrNull()
         
-        // Kategorileri al
         val tags = document.select(".cat-links a").map { it.text() }
-        
-        // Oyuncular/Yapımcılar
         val actors = document.select(".starring a").map { it.text() }
-
-        // Trailer linki
         val trailer = document.selectFirst("a[href*=youtube]")?.attr("href")
 
         val episodes = mutableListOf<Episode>()
 
-        // Eğer bölümlü bir yapı varsa (bazı belgeseller seri halinde olabilir)
         document.select(".episode-item").forEachIndexed { index, element ->
             val epName = element.selectFirst(".episode-title")?.text() ?: "Bölüm ${index + 1}"
             val epHref = fixUrl(element.selectFirst("a")?.attr("href") ?: return@forEachIndexed)
             val epPoster = fixUrlNull(element.selectFirst("img")?.attr("src"))
             
             episodes.add(
-                Episode(
-                    data = epHref,
-                    name = epName,
-                    season = 1,
-                    episode = index + 1,
+                newEpisode(epHref) {
+                    name = epName
+                    season = 1
+                    episode = index + 1
                     posterUrl = epPoster
-                )
+                }
             )
         }
 
@@ -112,56 +103,45 @@ class BelgeselIzlesene : MainAPI() {
     ): Boolean {
         val document = app.get(data).document
         
-        // Video kaynaklarını bul
         document.select("iframe").forEach { iframe ->
             val src = iframe.attr("src")
             if (src.isNotBlank()) {
-                // OEmbed veya direkt video linkleri
                 loadExtractor(src, data, subtitleCallback, callback)
             }
         }
 
-        // Alternatif kaynaklar (VK, Mail.ru, vb.)
         document.select(".video-source a").forEach { source ->
-            val videoUrl = source.attr("data-src") ?: source.attr("href")
+            val videoUrl = source.attr("data-src").ifBlank { source.attr("href") }
             if (videoUrl.isNotBlank()) {
                 loadExtractor(videoUrl, data, subtitleCallback, callback)
             }
         }
 
-        // Embed kodları içindeki linkler
         document.select(".entry-content").first()?.let { content ->
             val html = content.html()
             
-            // VK.com linkleri
             Regex("https?://vk\\.com/video[\\w\\-_]+").findAll(html).forEach { match ->
-                val vkUrl = match.value
-                loadExtractor(vkUrl, data, subtitleCallback, callback)
+                loadExtractor(match.value, data, subtitleCallback, callback)
             }
             
-            // Mail.ru linkleri
             Regex("https?://my\\.mail\\.ru/video/embed/[\\w\\-/]+").findAll(html).forEach { match ->
-                val mailruUrl = match.value
-                loadExtractor(mailruUrl, data, subtitleCallback, callback)
+                loadExtractor(match.value, data, subtitleCallback, callback)
             }
             
-            // Ok.ru linkleri
             Regex("https?://ok\\.ru/video/[\\d]+").findAll(html).forEach { match ->
-                val okUrl = match.value
-                loadExtractor(okUrl, data, subtitleCallback, callback)
+                loadExtractor(match.value, data, subtitleCallback, callback)
             }
             
-            // YouTube embedleri
             Regex("youtube\\.com/embed/([\\w\\-]+)").find(html)?.groupValues?.get(1)?.let { videoId ->
                 callback.invoke(
-                    ExtractorLink(
+                    newExtractorLink(
                         source = "YouTube",
                         name = "YouTube",
-                        url = "https://www.youtube.com/watch?v=$videoId",
-                        referer = "https://www.youtube.com/",
-                        quality = Qualities.Unknown.value,
-                        isM3u8 = false
-                    )
+                        url = "https://www.youtube.com/watch?v=$videoId"
+                    ) {
+                        referer = "https://www.youtube.com/"
+                        quality = Qualities.Unknown.value
+                    }
                 )
             }
         }
