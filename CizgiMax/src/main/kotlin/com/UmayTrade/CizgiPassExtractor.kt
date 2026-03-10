3fc33a61c8093d4bfa06241d3a3f2ea3c5178092
UmayTrade <kocatepe828@gmail.com>
2026-03-11 02:38:59 +0300

    Add files via upload

---
+++ CizgiMax/src/main/kotlin/com/UmayTrade/CizgiPassExtractor.kt
@@ -0,0 +1,8 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.UmayTrade
+
+class CizgiPass : CizgiDuo() {
+    override var name    = "CizgiPass"
+    override var mainUrl = "https://cizgipass5.online"
+}
