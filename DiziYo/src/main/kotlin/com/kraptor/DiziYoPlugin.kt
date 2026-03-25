fddc8dfe3e63b38ab3435d55007956d3a0d59c9d
UmayTrade <kocatepe828@gmail.com>
2026-03-26 02:39:43 +0300

    Update DiziYoPlugin.kt

--- DiziYo/src/main/kotlin/com/kraptor/DiziYoPlugin.kt
+++ DiziYo/src/main/kotlin/com/kraptor/DiziYoPlugin.kt
@@ -5,25 +5,9 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class DiziYoPlugin: Plugin() {
+class DiziYoPlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(DiziYo())
-        registerExtractorAPI(ContentX())
-        registerExtractorAPI(Hotlinger())
-        registerExtractorAPI(RapidVid())
-        registerExtractorAPI(TRsTX())
-        registerExtractorAPI(VidMoxy())
-        registerExtractorAPI(Sobreatsesuyp())
-        registerExtractorAPI(TurboImgz())
-        registerExtractorAPI(TurkeyPlayer())
-        registerExtractorAPI(Hotlinger())
-        registerExtractorAPI(FourCX())
-        registerExtractorAPI(PlayRu())
-        registerExtractorAPI(FourPlayRu())
-        registerExtractorAPI(FourPichive())
-        registerExtractorAPI(Pichive())
-        registerExtractorAPI(FourDplayer())
-        registerExtractorAPI(SNDplayer())
-        registerExtractorAPI(ORGDplayer())
+        // DiziYoUltimate olarak değiştirildi
+        registerMainAPI(DiziYoUltimate())
     }
 }
