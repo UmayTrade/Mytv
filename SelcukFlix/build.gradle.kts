fd750cce61c07e240b15adc3990b8d0a143030e6
UmayTrade <kocatepe828@gmail.com>
2026-04-29 03:58:54 +0300

    Update build.gradle.kts

--- SelcukFlix/build.gradle.kts
+++ SelcukFlix/build.gradle.kts
@@ -1,9 +1,9 @@
-version = 6
+version = 1
 
 cloudstream {
     authors     = listOf("UmayTrade")
     language    = "tr"
-    description = "Selcukflix'te her türden en yeni ve en popüler dizi ve filmleri izlemenin keyfini çıkarın. Aksiyondan romantiğe, bilim kurgudan dramaya, geniş kütüphanemizde herkes için bir şey var."
+    description = "Selcukflix'te her türden en yeni ve en popüler dizi ve filmleri izlemenin keyfini çıkarın. Aksiyondan romantiğe, bilim kurgudan dramaya, geniş kütüphanemizde herkes için bir şey var"
 
     /**
      * Status int as the following:
@@ -13,6 +13,6 @@ cloudstream {
      * 3: Beta only
     **/
     status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Movie", "TvSeries")
+    tvTypes = listOf("Movie","TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=selcukflix.com&sz=%size%"
 }
