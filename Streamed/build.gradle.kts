95b5506f02e8826b72eede083b90424dcee0c142
UmayTrade <kocatepe828@gmail.com>
2026-04-29 03:53:30 +0300

    Update build.gradle.kts

--- Streamed/build.gradle.kts
+++ Streamed/build.gradle.kts
@@ -1,11 +1,19 @@
-// ! Bu araç @Kraptor123 tarafından | @kekikanime için yazılmıştır.
-version = 25
+// ! Bu araç @SAKLImavi tarafından | @UmayTrade için yazılmıştır.
+version = 1
 
 cloudstream {
-    authors     = listOf("kraptor")
+    authors     = listOf("UmayTrade")
     language    = "en"
     description = "Streamed ile Canlı Spor Yayınlarını İzleyebilirsiniz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
     status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Live") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
+    tvTypes = listOf("Live")
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://streamed.pk/&size=128"
 }
