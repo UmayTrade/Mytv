248e49b21c346c4ae84fd0f89d6be9534307801b
UmayTrade <kocatepe828@gmail.com>
2026-04-29 04:01:26 +0300

    Update build.gradle.kts

--- Full4kizle/build.gradle.kts
+++ Full4kizle/build.gradle.kts
@@ -1,11 +1,19 @@
-// ! Bu araç @ByAyzen tarafından | @kekikanime için yazılmıştır.
-version = 9
+// ! Bu araç @SAKLImavi tarafından | @UmayTrade için yazılmıştır.
+version = 1
 
 cloudstream {
-    authors     = listOf("ByAyzen")
+    authors     = listOf("UmayTrade")
     language    = "tr"
     description = "Filmci Baba, film izleme sitesi 4k Full film izle, 1080p ve 4k (yalan) kalite de sinema filmleri ve dizileri, tek parça hd kalitede türkçe dublajlı filmler seyret."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
     status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Movie", "AsianDrama") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
+    tvTypes = listOf("Movie", "AsianDrama")
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://full4kizle.cc/&size=128"
 }
