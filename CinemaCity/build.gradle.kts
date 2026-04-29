60c5b28a038bbf4d2b0d1603a9f66002bee44670
UmayTrade <kocatepe828@gmail.com>
2026-04-29 04:09:25 +0300

    Update authors and version in build.gradle.kts

--- CinemaCity/build.gradle.kts
+++ CinemaCity/build.gradle.kts
@@ -1,11 +1,19 @@
-// ! Bu araç @Kraptor123 tarafından | @kekikanime için yazılmıştır.
-version = 4
+// ! Bu araç @SAKLImavi tarafından | @UmayTrade için yazılmıştır.
+version = 1
 
 cloudstream {
-    authors     = listOf("ByAyzen", "kraptor")
+    authors     = listOf("UmayTrade")
     language    = "en"
     description = "Cinema City."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
     status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Movie", "TvSeries", "Cartoon") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
+    tvTypes = listOf("Movie", "TvSeries", "Cartoon")
     iconUrl = "https://i.imgur.com/A87j6ue.png"
 }
