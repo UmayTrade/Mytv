4f0072a4cfc0bc8710b35fba6c0a0175e2365e5a
UmayTrade <kocatepe828@gmail.com>
2026-09-13 02:11:59 +0300

    Update LoveFilmPlugin.kt

--- LoveFilm/src/main/kotlin/com/UmayTrade/LoveFilmPlugin.kt
+++ LoveFilm/src/main/kotlin/com/UmayTrade/LoveFilmPlugin.kt
@@ -1,15 +1,13 @@
 package com.UmayTrade
 
 import android.content.Context
-import com.UmayTrade.VidMolyExtractor
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class LoveFilmPlugin: Plugin() {
+class LoveFilmPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(LoveFilm())
-
         registerExtractorAPI(VidMolyExtractor())
     }
 }
