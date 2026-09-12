4903a2e61e88a3eb7cb4783225b6571cfb914b81
UmayTrade <kocatepe828@gmail.com>
2026-09-13 02:56:31 +0300

    Update FilmMakinesiPlugin.kt

--- FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesiPlugin.kt
+++ FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesiPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class FilmMakinesiPlugin: Plugin() {
+class FilmMakinesiPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(FilmMakinesi())
         registerExtractorAPI(CloseLoadFm())
